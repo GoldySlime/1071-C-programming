d6822d68cb04836946433462358fb4cfa17b76af
GoldySlime <39287312+GoldySlime@users.noreply.github.com>
2018-10-03 15:17:20 +0800

    Update menu2.cpp

--- w04/menu2.cpp
+++ w04/menu2.cpp
@@ -1,118 +1,258 @@
 #include <stdio.h>
 
-void drawRect(int height, int width)
+void prchar(char c, int n)
 {
-    for(int i=1; i<=height ; ++i)
+    for(int i=1; i<=n; i++)
     {
-        for(int j=1; j<=width ; ++j)
-        {
-            if(i==1 || j==1 || i==height || j==width)
-                printf("*");
-            else
-                printf(" ");
-        }
-        printf("\n");
+        printf("%c",c);
     }
 }
 
-void drawVertT(int height, int type)
+void drawRect(int height, int width, int filled)
+{
+    if(filled)
+    {
+        for(int i=1; i<=height ; ++i)
+        {
+            for(int j=1; j<=width ; ++j)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        for(int i=1; i<=height ; ++i)
+        {
+            for(int j=1; j<=width ; ++j)
+            {
+                if(i==1 || j==1 || i==height || j==width)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+    }
+
+}
+
+void drawVertT(int height, int type, int filled)
 {
     switch(type)
     {
     case 1:
-        for(int i=1; i<=height ; ++i)
+    {
+        if(filled)
         {
-            for(int j=1; j<=i ; ++j)
+            for(int i=1; i<=height ; ++i)
             {
-                if(j==1 || j==i || j==height || i==height)
-                    printf("*");
-                else
-                    printf(" ");
+                prchar('*',i);
+                printf("\n");
             }
-            printf("\n");
+        }
+        else
+        {
+            for(int i=1; i<=height ; ++i)
+            {
+                if(i==1 || i==height)
+                {
+                    prchar('*',i);
+                }
+                else
+                {
+                    printf("*");
+                    prchar(' ',i-2);
+                    printf("*");
+                }
+                printf("\n");
+            }
         }
         break;
+    }
     case 2:
     {
-        for(int i=1 ; i<=height ; i++)
+        if(filled)
         {
-            for(int j=1 ; j<=height ; j++)
+            for(int i=1 ; i<=height ; i++)
             {
-                if(i==height || j==height || i==height || j==height-i+1)
-                    printf("*");
-                else
-                    printf(" ");
+                prchar(' ',height-i+1);
+                prchar('*',i);
+                printf("\n");
             }
-            printf("\n");
         }
-
+        else
+        {
+            for(int i=1 ; i<=height ; i++)
+            {
+                for(int j=1 ; j<=height ; j++)
+                {
+                    if(i==height || j==height || i==height || j==height-i+1)
+                        printf("*");
+                    else
+                        printf(" ");
+                }
+                printf("\n");
+            }
+        }
     }
     break;
     case 3:
     {
-        for(int i=1 ; i<=height ; i++)
+        if(filled)
         {
-            for(int j=1 ; j<=height ; j++)
+            for(int i=1 ; i<=height ; i++)
             {
-                if(j==i || j==height || i==1)
-                    printf("*");
-                else
-                    printf(" ");
+                prchar('*',height-i+1);
+                printf("\n");
             }
-            printf("\n");
         }
-
+        else
+        {
+            for(int i=1 ; i<=height ; i++)
+            {
+                for(int j=i ; j<=height ; j++)
+                {
+                    if(i==1 || j==i || j==height)
+                        printf("*");
+                    else
+                        printf(" ");
+                }
+                printf("\n");
+            }
+        }
     }
     break;
     case 4:
     {
-        for(int i=1 ; i<=height ; i++)
+        if(filled)
         {
-            for(int j=i ; j<=height ; j++)
+            for(int i=1 ; i<=height ; i++)
             {
-                if(i==1 || j==i || j==height)
-                    printf("*");
-                else
-                    printf(" ");
+                prchar(' ',i);
+                prchar('*',height-i+1);
+                printf("\n");
             }
-            printf("\n");
         }
-
+        else
+        {
+            for(int i=1 ; i<=height ; i++)
+            {
+                for(int j=1 ; j<=height ; j++)
+                {
+                    if(j==i || j==height || i==1)
+                        printf("*");
+                    else
+                        printf(" ");
+                }
+                printf("\n");
+            }
+        }
     }
     }
 }
+
+void drawTri(int height, int type, int filled)
+{
+    switch(type)
+    {
+    case 1:
+    {
+        if(filled)
+        {
+            for(int i=1 ; i<=height ; i++)
+            {
+                prchar(' ',height-i+1);
+                prchar('*',(i*2)-1);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=1 ; i<=height ; i++)
+            {
+                for(int j=1 ; j<=height*2-1 ; j++)
+                {
+                    if(i==height|| i==height-j+1 || j==height+i-1 )
+                        printf("*");
+                    else
+                        printf(" ");
+                }
+                printf("\n");
+            }
+        }
+    }
+    break;
+    case 2:
+    {
+        if(filled)
+        {
+            for(int i=1 ; i<=height ; i++)
+            {
+                prchar(' ',i-1);
+                prchar('*',2*(height-i)+1);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=1 ; i<=height ; i++)
+            {
+                for(int j=1 ; j<=height*2-1 ; j++)
+                {
+                    if(j==i || i==1 || i==height*2-j )
+                        printf("*");
+                    else
+                        printf(" ");
+                }
+                printf("\n");
+            }
+        }
+    }
+    break;
+    }
+}
+
 int main()
 {
 
-        int choice;
-        int height;
-        int width;
-        int type;
+    int choice;
+    int height;
+    int width;
+    int type;
+    int filled;
 
-        while(1)
+    while(1)
+    {
+        printf("Main Menu\n");
+        printf("1. Rectangle\n");
+        printf("2. Vertical Triangle\n");
+        printf("3. Tsosceles Triangle\n");
+        printf("4. Exit\n");
+        printf("=> ");
+        scanf("%d", &choice);
+        if(choice==4)
+            break;
+        switch(choice)
         {
-            printf("Main Menu\n");
-            printf("1. Rectangle\n");
-            printf("2. Vertical Triangle\n");
-            printf("3. Exit\n");
-            printf("=> ");
-            scanf("%d", &choice);
-            if(choice==3)break;
-            switch(choice)
-            {
-            case 1:
-                printf("Enter Height and width: ");
-                scanf("%d %d", &height, &width);
-                drawRect(height,width);
-                break;
-            case 2:
-                printf("Enter Height and type: ");
-                scanf("%d %d", &height, &type);
-                drawVertT(height,type);
-                break;
-            }
+        case 1:
+            printf("Enter Height, width, filled: ");
+            scanf("%d %d %d", &height, &width, &filled);
+            drawRect(height,width,filled);
+            break;
+        case 2:
+            printf("Enter Height, type, filled: ");
+            scanf("%d %d %d", &height, &type, &filled);
+            drawVertT(height,type,filled);
+            break;
+        case 3:
+            printf("Enter Height, type, filled: ");
+            scanf("%d %d %d", &height, &type, &filled);
+            drawTri(height,type,filled);
+            break;
         }
-        return 0;
+    }
+    return 0;
 }
-
 
